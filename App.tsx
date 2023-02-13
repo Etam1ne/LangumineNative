286858690c5e2d4e3a71c5bf88ca1646c0b3d818
Etamine <etam1ne@yandex.ru>
2023-02-13 20:52:29 +0300

    project init

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { StyledContainer } from './styles/Container.styled';
+import { StyledNavBar } from './styles/NavBar.styled';
+
+export default function App() {
+  return (
+    <StyledContainer>
+      <StyledNavBar>
+
+      </StyledNavBar>
+    </StyledContainer>
+  );
+}
---
+++ styles/NavBar.styled.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const StyledNavBar = styled.View`
+    height: 10%;
+    background-color: green;
+`;
